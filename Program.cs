9d87180cb39d6e7742c5fb4ccb145d17d051b5d2
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-22 16:17:39 -0400

    comments on code

--- Program.cs
+++ Program.cs
@@ -10,17 +10,17 @@ namespace Operators
             var a = 1;
             var b = 2;
             var c = 3;
-            //Console.WriteLine(a + b); = 13
-            //Console.WriteLine(a / b); = 3. We get a whole number because we have not specified we want to get a float. See below.
-            //Console.WriteLine((float)a / (float)b); = 3.3333
-            //Console.WriteLine((a + b) * c); = 9
-            //Console.WriteLine(a > b); = False
-            //Console.WriteLine(a == b); = False
-            //Console.WriteLine(a != b); = True
-            //Console.WriteLine(c > b && c > a); = True
-            //Console.WriteLine(c > b && c == a); = False
-            //Console.WriteLine(c > b || c == a); = True
-            //Console.WriteLine(!(c > b || c == a)); = False
+            /* Console.WriteLine(a + b); = 13
+            Console.WriteLine(a / b); = 3. We get a whole number because we have not specified we want to get a float. See below.
+            Console.WriteLine((float)a / (float)b); = 3.3333
+            Console.WriteLine((a + b) * c); = 9
+            Console.WriteLine(a > b); = False
+            Console.WriteLine(a == b); = False
+            Console.WriteLine(a != b); = True
+            Console.WriteLine(c > b && c > a); = True
+            Console.WriteLine(c > b && c == a); = False
+            Console.WriteLine(c > b || c == a); = True
+            Console.WriteLine(!(c > b || c == a)); = False */
         }
     }
 }
