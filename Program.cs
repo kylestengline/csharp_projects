22b355486588f8ba9e64c1f32ab45f73a1fb4dfe
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-22 16:16:35 -0400

    adds operators exercise

--- Program.cs
+++ Program.cs
@@ -1,33 +1,26 @@
 ﻿using System;
 
-namespace TypeConversion
+namespace Operators
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-        //Explicit type conversion
-
-            //int i = 1000;
-            //must explicitly state casting if there's a chance for data loss.
-            //byte b = (byte) i;
-            //Console.WriteLine(b);
-            
-            //Non-Compatible Types
-            try
-            {
-                //var number = "1234";
-                //byte b = Convert.ToByte(number);
-               // Console.WriteLine(b);
-                
-                string str = "true";
-                bool b = Convert.ToBoolean(str);
-                Console.WriteLine(b);
-            }
-            catch (System.Exception)
-            {
-                Console.WriteLine("The number could not be converted to a byte");
-            }
+            //Arithmetic Operators 
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            //Console.WriteLine(a + b); = 13
+            //Console.WriteLine(a / b); = 3. We get a whole number because we have not specified we want to get a float. See below.
+            //Console.WriteLine((float)a / (float)b); = 3.3333
+            //Console.WriteLine((a + b) * c); = 9
+            //Console.WriteLine(a > b); = False
+            //Console.WriteLine(a == b); = False
+            //Console.WriteLine(a != b); = True
+            //Console.WriteLine(c > b && c > a); = True
+            //Console.WriteLine(c > b && c == a); = False
+            //Console.WriteLine(c > b || c == a); = True
+            //Console.WriteLine(!(c > b || c == a)); = False
         }
     }
 }
