1e69ca9db005a797aa4787c45affd368d96b51cb
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-22 19:10:50 -0400

    adds string concatenation exercise

--- Program.cs
+++ Program.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 
-namespace Arrays
+namespace Strings
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            //int[] numbers = new int[3] 
-            //short hand for the above:
-            var numbers = new int[3];
-            numbers[0] = 1;
-
-            System.Console.WriteLine(numbers[0]);
-            System.Console.WriteLine(numbers[1]);
-            System.Console.WriteLine(numbers[2]);
+            var firstName = "Kyle";
+            var lastName = "Wins";
             
-            //declaring array and setting the size
-            var flags = new bool[3];
-            flags[0] = true;
+            var fullName = firstName + " " + lastName;
             
-            System.Console.WriteLine(flags[0]);
-            System.Console.WriteLine(flags[1]);
-            System.Console.WriteLine(flags[2]);
+            //An easier way to write the above:
+            var myFullName = string.Format("My name is {0}{1}", firstName, lastName);
+            //0 is replaced with first name while 1 is replaced by lastname.
             
-/*Object Initialization Syntax means to supply values for the each element of the array*/
-            var names = new string[3] {"Jack", "John", "Mary"};
+            var names = new string[3] { "John", "Jill", "Jack" };
+            
+            // first argument of .Join is the separator then the array.
+            var formattedName = string.Join(",", names);
+            System.Console.WriteLine(formattedName);
+            
+            /* var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\\folder3";
+            System.Console.WriteLine(text); */
+            
+            /*Another easier way to write the above:
+            Similar to heredoc syntax */
+            var text = @"Hi John
+            Look into the following paths:
+            c:\folder1\folder2
+            c:\folder3\folder4";
+            System.Console.WriteLine(text);
         }
     }
 }
