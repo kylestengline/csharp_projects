b1fb33ffa918dc67e936b47ef054f7f9e131c95e
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-22 14:53:47 -0400

    adds exercise with variables and contstants

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace Variables
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'A';
+            //string firstName = "Kyle";
+            //bool isWorking = false; 
+            
+            // we can use var when we want to generalize our datatypes, but be more specific when we want to use bytes, int, etc..
+            //var number = 2;
+            //var count = 10;
+            //var totalPrice = 20.95f;
+            //var character = 'A';
+            //var firstName = "Kyle";
+            //var isWorking = false; 
+            
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(character);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+            
+            //Finding the range we can store
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            
+            //Constants
+            const float Pi = 3.14f;
         }
     }
 }
