cc061ef366118c6470e61779bdca403cee17b79a
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-22 18:50:08 -0400

    adds arrays exercise

--- Program.cs
+++ Program.cs
@@ -1,21 +1,30 @@
 ﻿using System;
-//Using the below, this is how I can reference the methods in math
-using csharp_projects.Math;
 
-namespace Classes
+namespace Arrays
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            //int[] numbers = new int[3] 
+            //short hand for the above:
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            System.Console.WriteLine(numbers[0]);
+            System.Console.WriteLine(numbers[1]);
+            System.Console.WriteLine(numbers[2]);
+            
+            //declaring array and setting the size
+            var flags = new bool[3];
+            flags[0] = true;
+            
+            System.Console.WriteLine(flags[0]);
+            System.Console.WriteLine(flags[1]);
+            System.Console.WriteLine(flags[2]);
+            
+/*Object Initialization Syntax means to supply values for the each element of the array*/
+            var names = new string[3] {"Jack", "John", "Mary"};
         }
     }
 }
