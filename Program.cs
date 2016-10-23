8001513194b57270a5f5890d2dd5d8f7d39a95bf
Kyle Stengline <kyle.stengline@gmail.com>
2016-10-22 20:00:00 -0400

    adds enum exercise

--- Program.cs
+++ Program.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 
-namespace Strings
+namespace Enums
 {
+//Because enum is a new type we need to define it at the namespace level.
+    public enum ShippingMethod
+    //always set values as it is convention. 
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
     class Program
     {
         public static void Main(string[] args)
         {
-            var firstName = "Kyle";
-            var lastName = "Wins";
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method); //prints 3
             
-            var fullName = firstName + " " + lastName;
+            var methodId = 3;
+            System.Console.WriteLine((ShippingMethod)methodId); //prints Express
             
-            //An easier way to write the above:
-            var myFullName = string.Format("My name is {0}{1}", firstName, lastName);
-            //0 is replaced with first name while 1 is replaced by lastname.
+            System.Console.WriteLine(method.ToString()); //prints Express
             
-            var names = new string[3] { "John", "Jill", "Jack" };
-            
-            // first argument of .Join is the separator then the array.
-            var formattedName = string.Join(",", names);
-            System.Console.WriteLine(formattedName);
-            
-            /* var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\\folder3";
-            System.Console.WriteLine(text); */
-            
-            /*Another easier way to write the above:
-            Similar to heredoc syntax */
-            var text = @"Hi John
-            Look into the following paths:
-            c:\folder1\folder2
-            c:\folder3\folder4";
-            System.Console.WriteLine(text);
+            //This is how you convert or parse a string to a enum.
+            var methodName = "Express";
+            /* first setting var then casting because parse returns an object and we need an enum. This is why we need to cast */
+            var shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+            System.Console.WriteLine(shippingMethod);
         }
     }
 }
