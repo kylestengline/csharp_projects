44d7d7f6363a5ad9f0cda02abcc5f62a620ede52
Kyle Stengline <kyle.stengline@gmail.com>
2016-11-06 15:57:30 -0500

    adds conditional exercises with switch, if else and ternary

--- Program.cs
+++ Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
 
-namespace ConsoleApplication
+namespace Conditionals
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+      //Switch Case Statement
+      var season = Season.Autumn;
+      switch (season)
+      {
+        //If autumn and summer had the same message, we can put the case statements on top of each other like so.
+        case Season.Autumn:
+        case Season.Summer:
+          Console.WriteLine("We've got a promotion");
+          break;
+          
+        default:
+          Console.WriteLine("I don't understand that season.");
+          break;
+      }
+      
+     //Basic if/else 
+     /* int hour = 10;
+
+      if (hour > 0 && hour < 12)
+      {
+        Console.WriteLine("It's Morning.");
+      }
+      else if (hour >= 12 && hour < 18)
+      {
+        Console.WriteLine("It's Afternoon.");
+      }
+      else 
+      {
+        Console.WriteLine("It's Evening.");
+      }*/
+      
+      //Condition Statment for short if /else
+      
+      //bool isGoldCustomer = true;
+
+      //if/else 
+      /*float price;
+      if (isGoldCustomer)
+        price = 19.95f;
+      else
+        price = 29.95f;*/
+
+      //conditional statement similar to ternary operator. We seperate conditions with a ? and the : is similar to an else. After ? it's: if isGoldCustomer is true then price is 19.95 :(else) price is 29.95
+      //shortcut for if/else
+      
+      //float price = (isGoldCustomer) ? 19.95f : 29.95f;
+      //Console.WriteLine(price);
+
+      
+      
     }
+  }
 }
