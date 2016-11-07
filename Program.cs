63a462e31d0b96ac738dcce30a4e2b447e0e7fa4
Kyle Stengline <kyle.stengline@gmail.com>
2016-11-06 19:16:21 -0500

    finishes conditional exercises

--- Program.cs
+++ Program.cs
@@ -7,39 +7,72 @@ namespace ConditionalExercise
     public static void Main(string[] args)
     {
       // Display valid or invalid number depending on number input
-      System.Console.Write("\nPlease enter a number between 1 and 10: ");
-      String a = Console.ReadLine();
-      int b = Convert.ToInt32(a);
+      public void Numbers()
+      {
+        System.Console.Write("\nPlease enter a number between 1 and 10: ");
+        String a = Console.ReadLine();
+        int b = Convert.ToInt32(a);
+        
+        if (b > 10 && b < 1)
+        {
+          Console.WriteLine("Invalid Number");
+        }
+        else
+        {
+          Console.WriteLine("Valid Number");
+        }
+      }
       
-      if (b > 10 && b < 1)
-      {
-        Console.WriteLine("Invalid Number");
-      }
-      else
-      {
-        Console.WriteLine("Valid Number");
-      }
-
        //Display max between two numbers
-      Console.Write("\nPick A number: ");
-      var first = Convert.ToInt32(Console.ReadLine());
-      
-      Console.Write("\nPick Another number: ");
-      var second = Convert.ToInt32(Console.ReadLine());
+      public void Max()
+      {
+        Console.Write("\nPick A number: ");
+        var first = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("\nPick Another number: ");
+        var second = Convert.ToInt32(Console.ReadLine());
 
-      var max = (first > second) ? first : second;
-      Console.WriteLine("Max: " + max);
-      
-      Console.Write("\nHeight of Image: ");
-      var height = Convert.ToInt32(Console.ReadLine());
+        var max = (first > second) ? first : second;
+        Console.WriteLine("Max: " + max);
+        
+      }
 
-      Console.WriteLine("\nWidth of Image: ");
-      var width = Convert.ToInt32(Console.ReadLine());
+      public void Image()
+      {
+        Console.Write("\nHeight of Image: ");
+        var height = Convert.ToInt32(Console.ReadLine());
 
-      if (width > height)
-        Console.WriteLine("Landscape");
-      else 
-        Console.WriteLine("Portait");
+        Console.WriteLine("\nWidth of Image: ");
+        var width = Convert.ToInt32(Console.ReadLine());
+        
+        var orientation = (width > height) ? "Landscape" : "Portrait";
+      }  
+
+      public void Camera()
+      {
+        Console.Write("\nEnter Speed Limit: ");
+        var speedLimit = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("\nEnter the speed of the car: ");
+        var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+        var demerits = 0;
+        if (carSpeed < speedLimit)
+        {
+          Console.WriteLine("OK");
+        }
+         else
+        {
+          //setting points at 5 for violation
+          const int kmPerDemeritPoint = 5;
+          //subtract carSpeed with limt then divide.
+          var demeritPoints = (carSpeed - speedLimit)/kmPerDemeritPoint;
+          if (demeritPoints > 12)
+              Console.WriteLine("License Suspended");
+          else
+              Console.WriteLine("Demerit points: " + demeritPoints);
+        } 
+      }
     }
   }
 }
