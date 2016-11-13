fd4bb1f09432164b93dcbf1269f7c711c44c0b29
Kyle Stengline <kyle.stengline@gmail.com>
2016-11-12 19:46:25 -0500

    adds lists demo

--- Program.cs
+++ Program.cs
@@ -1,51 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleApplication
+namespace Lists
 {
   public class Program
   {
     public static void Main(string[] args)
     {
-      var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+      //the int is telling you what kind of list it is. We could also say new List<String>() if we wanted.
+      //We don't have to initialize the list right now. Just using it for this example.
+      var numbers = new List<int>() {1, 2, 3, 4};
+      numbers.Add(1);
+      numbers.AddRange(new int[3] {5, 6, 7});
 
-      //Length - returns size of array.
-      System.Console.WriteLine("Length");
-      System.Console.WriteLine("Length: " + numbers.Length);
+      foreach(var num in numbers)
+        System.Console.WriteLine(num); //prints 1, 2, 3, 4, 1, 5, 6, 7
 
-      //IndexOf() gets the index for the number you pass in.
-      System.Console.WriteLine("IndexOf()");
-      var index = Array.IndexOf(numbers, 9); //prints 2 because the index of the number 9 is 2.
-      System.Console.WriteLine("Index of 9: " + index);
+      System.Console.WriteLine("IndexOf on Lists");
+      System.Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+      System.Console.WriteLine("Index of 1: " + numbers.LastIndexOf(1));
 
-      //Clear() will clear elements in the array. We pass in the name of the array with the index we want to start at and end with.
-      System.Console.WriteLine("Clear()");
-      Array.Clear(numbers, 0, 2);
-      foreach(var n in numbers)
-        System.Console.WriteLine(n);
-        //prints the first 2 numbers in numbers as 0
 
-        //Copy() will copy an array. It takes 3 parametes. 1. source array 2. destination. 3. number of elements we would like to copy.
-      System.Console.WriteLine("Copy()");
-      int[] another = new int[3];
-      Array.Copy(numbers, another, 3);
+      System.Console.WriteLine("Count");
+      System.Console.WriteLine("Count: " + numbers.Count);
 
-      foreach(var num in another)
-        System.Console.WriteLine(num);
-
-      //Sort()
-      System.Console.WriteLine("Sort()");
-      Array.Sort(numbers);
-
-      foreach(var n in numbers)
-        System.Console.WriteLine(n);
-
-      //Reverse()
-      System.Console.WriteLine("Reverse()");
-      
-      Array.Reverse(numbers);
+      System.Console.WriteLine("Remove");
+      for (var i = 0; i < numbers.Count; i++)
+      {
+        if (numbers[i] == 1)
+          numbers.Remove(numbers[i]);
+      }
       foreach(var num in numbers)
         System.Console.WriteLine(num);
+
+      System.Console.WriteLine("Clear");
+      
+      numbers.Clear();
+      System.Console.WriteLine("Count: " + numbers.Count);
     }
   }
 }
-//Keep in mind that certain methods can only be called on array classes and others can only be called objects. See IndexOf compared to Length
